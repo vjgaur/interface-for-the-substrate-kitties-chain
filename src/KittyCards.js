1d384240f2392b78a3321f45c668b72f812d2fac
vjgaur <vijayendra.gaur@gmail.com>
2022-01-25 11:39:03 +0530

    added kittycards.js componenet that renders a grid for kittycard

---
+++ src/KittyCards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  Button,
+  Card,
+  Grid,
+  Message,
+  Modal,
+  Form,
+  Label,
+} from 'semantic-ui-react'
+
+import KittyAvatar from './KittyAvatar'
+import { TxButton } from './substrate-lib/components'
